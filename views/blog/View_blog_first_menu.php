f19c2a28f2f6f7a8d9785031a94cf6e0138104fa
Sergey Gulidov <gulidovs9@gmail.com>
2012-01-14 04:54:32 +0200

    first commit

---
+++ views/blog/View_blog_first_menu.php
@@ -0,0 +1,15 @@
+<div id="first_menu">
+ <a href=''>Создать блог</a>
+ <a href=''>Создать пост</a>
+    <form action="">
+        <label>
+            Войти в блог
+            <select value='Название блога'>
+                <?php for($i=0;;){?>
+                <option >
+
+                </option>
+                <?}?>
+        </label>
+    </form>
+</div>
